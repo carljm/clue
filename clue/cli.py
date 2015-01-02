64220c02646c8e8cd00fca2b8d6f2603da7edea9
Carl Meyer <carl@oddbird.net>
2015-01-01 19:50:58 -0500

    Add cli.parse_player.

--- clue/cli.py
+++ clue/cli.py
@@ -1,15 +1,32 @@
 from .cards import DECK
 
 
-class UnknownCardError(Exception):
+class CliError(Exception):
+    pass
+
+
+class UnknownCardError(CliError):
+    pass
+
+
+class UnknownPlayerError(CliError):
     pass
 
 
 def parse_card(prefix):
+    return _parse(prefix, DECK, UnknownCardError, 'card')
+
+
+def parse_player(prefix, names):
+    return _parse(prefix, names, UnknownPlayerError, 'player')
+
+
+def _parse(prefix, options, exception_class, option_type):
     lowered = prefix.lower()
-    candidates = [c for c in DECK if c.startswith(lowered)]
+    candidates = [o for o in options if o.lower().startswith(lowered)]
     if len(candidates) > 1:
-        raise UnknownCardError("More than one card matches %s." % prefix)
+        raise exception_class(
+            "More than one %s matches %s." % (option_type, prefix))
     elif not candidates:
-        raise UnknownCardError("No cards match %s." % prefix)
+        raise exception_class("No %s matches %s." % (option_type, prefix))
     return candidates[0]
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -20,3 +20,22 @@ class TestParseCard:
     def test_unknown(self):
         with pytest.raises(cli.UnknownCardError):
             cli.parse_card('foo')
+
+
+class TestParsePlayer:
+    def test_prefix(self):
+        assert cli.parse_player('c', ['Carl', 'Kathy']) == 'Carl'
+
+    def test_full(self):
+        assert cli.parse_player('Kathy', ['Carl', 'Kathy']) == 'Kathy'
+
+    def test_case_insensitive(self):
+        assert cli.parse_player('kath', ['Carl', 'Kathy']) == 'Kathy'
+
+    def test_ambiguous(self):
+        with pytest.raises(cli.UnknownPlayerError):
+            cli.parse_player('k', ['Kathy', 'Karissa'])
+
+    def test_unknown(self):
+        with pytest.raises(cli.UnknownPlayerError):
+            cli.parse_player('foo', ['Carl', 'Kathy'])
