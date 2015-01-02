181d634bed11a53fa5b773ccefe238cbb1ff833f
Carl Meyer <carl@oddbird.net>
2015-01-01 19:39:40 -0500

    Add cards.

---
+++ clue/cards.py
@@ -0,0 +1,31 @@
+ROOMS = [
+    'hall',
+    'dining room',
+    'billiard room',
+    'ballroom',
+    'kitchen',
+    'study',
+    'conservatory',
+    'lounge',
+    'library',
+]
+
+PEOPLE = [
+    'green',
+    'mustard',
+    'scarlet',
+    'plum',
+    'white',
+    'peacock',
+]
+
+WEAPONS = [
+    'revolver',
+    'rope',
+    'lead pipe',
+    'wrench',
+    'candlestick',
+    'knife',
+]
+
+DECK = ROOMS + PEOPLE + WEAPONS
